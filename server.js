657f0d715c05afa893bf15579da642cea818606d
Pooja Belbase <poojagiriraj.belbase@gmail.com>
2025-11-11 00:58:08 +0530

    Fixed cors issue

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://tekmiz-e-learning-platform.vercel.app/'],
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://tekmiz-e-learning-platform.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
